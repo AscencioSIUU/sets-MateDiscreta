c6a68802a740b783e6b87cbe4ad60f058e84f2b1
SebastianUVG <gar22291@uvg.edu.gt>
2024-08-21 17:32:25 -0600

    Funciones del programa
    Se agrego la funcion de intereseccion, diferencia, diferencia Simetrica y complemento

--- src/operations.py
+++ src/operations.py
@@ -1,10 +1,16 @@
 def universo():
     abecedario_mayusculas = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
     numeros_0_9 = [str(i) for i in range(10)]
-    print(abecedario_mayusculas)
-    print(numeros_0_9)
+    return abecedario_mayusculas + numeros_0_9
 
-
+def complemento(conjunto):
+    # Obtenemos el universo completo
+    universo_completo = universo()
+    
+    # Calculamos el complemento restando los elementos del conjunto dado al universo
+    complemento_resultado = [elemento for elemento in universo_completo if elemento not in conjunto]
+    
+    return complemento_resultado
 
 def union(ConjuntoA, ConjuntoB):
     Final = list(ConjuntoA)
@@ -18,13 +24,37 @@ def union(ConjuntoA, ConjuntoB):
     return Final
 
 
-
+def interseccion(ConjuntoA, ConjuntoB):
+    return [elemento for elemento in ConjuntoA if elemento in ConjuntoB]
     
     
+def diferencia(ConjuntoA, ConjuntoB):
+    # Calculamos la diferencia incluyendo solo los elementos de ConjuntoA que no están en ConjuntoB
+    diferencia_resultado = [elemento for elemento in ConjuntoA if elemento not in ConjuntoB]
+    
+    return diferencia_resultado
 
 
-conjunto1 = ["a", "b", "c","1","2","3"]
-conjunto2 = ["a", "b", "c","4","5","6"]
+def diferencia_simetrica(ConjuntoA, ConjuntoB):
+    # Calculamos A - B
+    diferencia_A_B = diferencia(ConjuntoA, ConjuntoB)
+    
+    # Calculamos B - A
+    diferencia_B_A = diferencia(ConjuntoB, ConjuntoA)
+    
+    # La diferencia simétrica es la unión de A - B y B - A
+    diferencia_simetrica_resultado = diferencia_A_B + diferencia_B_A
+    
+    return diferencia_simetrica_resultado
+
+conjunto1 = ["A", "B", "C","1","2","3"]
+conjunto2 = ["A", "B", "C","4","5","6"]
 
 print(union(conjunto1,conjunto2))
+print(interseccion(conjunto1,conjunto2))
+print(complemento(conjunto1))
+print(diferencia(conjunto1,conjunto2))
+print(diferencia_simetrica(conjunto1,conjunto2))
 
+#https://www.disfrutalasmatematicas.com/conjuntos/calculadora-conjuntos.html
+#https://calculadoradeconjuntos.site
