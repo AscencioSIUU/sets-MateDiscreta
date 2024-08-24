d9eb377998041c1fe70805c826a963f597bd78e1
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2024-08-23 22:16:51 -0600

    Documentación y correción en main

--- src/menu.py
+++ src/menu.py
@@ -17,7 +17,7 @@ def menu():
         print("1. Construir Conjuntos") 
         print("2. Operar conjuntos") 
         print("3. Finalizar") 
-        choice = input("> ¿Qué desea hacer? :")
+        choice = input("> ¿Qué desea hacer? : ")
         
         if choice == '1':
             print("Construir Conjuntos")
@@ -30,20 +30,21 @@ def menu():
                 
         
         elif choice == '2':
-            if conjuntoA is None and conjuntoB is None and conjuntoC is None:
-                print("Ingresa primero los conjuntos.")
-                
+            # Valida que se hayan ingresado los conjuntos
+            if conjuntoA is None or conjuntoB is None:
+                print("Ingresa primero los conjuntos A y B.")
+           # Realiza las diferentes opreaciones
             else:
                 print(f"Conjunto A: {conjuntoA}")
                 print(f"Conjunto B: {conjuntoB}")
                 if conjuntoC:
                     print(f"Conjunto C: {conjuntoC}")
                 operation = input("¿Qué operación desea realizar?: \n1. Universo\n2. Complemento\n3. Unión\n4. Intersección\n5. Diferencia\n6. Diferencia Simétrica\n> Operación a realizar: ")
-                
+                # Realiza operacion de Universo
                 if operation == '1':
                     print(f"Conjunto Universo: {op.universo()}")
-                    limpiar_consola()
-                
+                    
+                # Realiza operacion de complemento
                 elif operation == '2':
                     print("Seleccione el conjunto para el complemento:")
                     if conjuntoA:
@@ -63,122 +64,114 @@ def menu():
                          print(f"Complemento de C: {op.complemento(conjuntoC)}")
                     else:
                         print("Selección no válida o conjunto no disponible.")
-
+ 
+                # Realiza operacion de unión
                 elif operation == '3':
                     print("Seleccione los conjuntos para la unión:")
-                    conjuntos = []
-                    if conjuntoA:
-                        print("1. Conjunto A")
-                    if conjuntoB:
-                        print("2. Conjunto B")
-                    if conjuntoC:
-                        print("3. Conjunto C")
+                    if conjuntoA and conjuntoB:
+                        print("1. Unión A ∪ B")
+                    if conjuntoA and conjuntoC:
+                        print("2. Unión A ∪ C")
+                    if conjuntoB and conjuntoC:
+                        print("3. Unión B ∪ C")
                     
-                    seleccion = input("> Ingrese los números de los conjuntos separados por comas (ejemplo: 1,2): ")
-                    seleccion = seleccion.split(',')
+                    seleccion = input("> Ingrese el número de la operación: ")
 
-                    for sel in seleccion:
-                        if sel == '1' and conjuntoA:
-                            conjuntos.append(conjuntoA)
-                        elif sel == '2' and conjuntoB:
-                            conjuntos.append(conjuntoB)
-                        elif sel == '3' and conjuntoC:
-                            conjuntos.append(conjuntoC)
-                    
-                    if conjuntos:
-                        union = op.union(*conjuntos)
-                        print(f"Unión de los conjuntos seleccionados: {union}")
+                    if seleccion == '1':
+                        print(f"Unión A ∪ B: {op.union(conjuntoA, conjuntoB)}")
+                    elif seleccion == '2' and conjuntoC:
+                        print(f"Unión A ∪ C: {op.union(conjuntoA, conjuntoC)}")
+                    elif seleccion == '3' and conjuntoC:
+                        print(f"Unión B ∪ C: {op.union(conjuntoB, conjuntoC)}")
                     else:
-                        print("No se seleccionaron conjuntos válidos.")   
-
+                        print("Selección no válida o conjuntos no disponibles.")
+  
+                # Realiza operacion de intersección
                 elif operation == '4':
                     print("Seleccione los conjuntos para la intersección:")
-                    conjuntos = []
-                    if conjuntoA:
-                        print("1. Conjunto A")
-                    if conjuntoB:
-                        print("2. Conjunto B")
-                    if conjuntoC:
-                        print("3. Conjunto C")
+                    if conjuntoA and conjuntoB:
+                        print("1. Intersección A ∩ B")
+                    if conjuntoA and conjuntoC:
+                        print("2. Intersección A ∩ C")
+                    if conjuntoB and conjuntoC:
+                        print("3. Intersección B ∩ C")
                     
-                    seleccion = input("> Ingrese los números de los conjuntos separados por comas (ejemplo: 1,2): ")
-                    seleccion = seleccion.split(',')
+                    seleccion = input("> Ingrese el número de la operación: ")
 
-                    for sel in seleccion:
-                        if sel == '1' and conjuntoA:
-                            conjuntos.append(conjuntoA)
-                        elif sel == '2' and conjuntoB:
-                            conjuntos.append(conjuntoB)
-                        elif sel == '3' and conjuntoC:
-                            conjuntos.append(conjuntoC)
-                    
-                    if conjuntos:
-                        interseccion = op.interseccion(*conjuntos)
-                        print(f"Intersección de los conjuntos seleccionados: {interseccion}")
+                    if seleccion == '1':
+                        print(f"Intersección A ∩ B: {op.interseccion(conjuntoA, conjuntoB)}")
+                    elif seleccion == '2' and conjuntoC:
+                        print(f"Intersección A ∩ C: {op.interseccion(conjuntoA, conjuntoC)}")
+                    elif seleccion == '3' and conjuntoC:
+                        print(f"Intersección B ∩ C: {op.interseccion(conjuntoB, conjuntoC)}")
                     else:
-                        print("No se seleccionaron conjuntos válidos.")
+                        print("Selección no válida o conjuntos no disponibles.")
 
+                # Realiza operacion de diferencia
                 elif operation == '5':
                     print("Seleccione los conjuntos para la diferencia:")
                     if conjuntoA and conjuntoB:
                         print("1. Diferencia A - B")
                         print("2. Diferencia B - A")
-                        if conjuntoC:
-                            print("3. Diferencia A - C")
-                            print("4. Diferencia B - C")
-                            print("5. Diferencia C - A")
-                            print("6. Diferencia C - B")
+                    if conjuntoA and conjuntoC:
+                        print("3. Diferencia A - C")
+                    if conjuntoB and conjuntoC:
+                        print("4. Diferencia B - C")
+                    if conjuntoC and conjuntoA:
+                        print("5. Diferencia C - A")
+                    if conjuntoC and conjuntoB:
+                        print("6. Diferencia C - B")
 
-                        seleccion = input("> Ingrese el número de la operación: ")
+                    seleccion = input("> Ingrese el número de la operación: ")
 
-                        if seleccion == '1':
-                            print(f"Diferencia A - B: {op.diferencia(conjuntoA, conjuntoB)}")
-                        elif seleccion == '2':
-                            print(f"Diferencia B - A: {op.diferencia(conjuntoB, conjuntoA)}")
-                        elif seleccion == '3' and conjuntoC:
-                            print(f"Diferencia A - C: {op.diferencia(conjuntoA, conjuntoC)}")
-                        elif seleccion == '4' and conjuntoC:
-                            print(f"Diferencia B - C: {op.diferencia(conjuntoB, conjuntoC)}")
-                        elif seleccion == '5' and conjuntoC:
-                            print(f"Diferencia C - A: {op.diferencia(conjuntoC, conjuntoA)}")
-                        elif seleccion == '6' and conjuntoC:
-                            print(f"Diferencia C - B: {op.diferencia(conjuntoC, conjuntoB)}")
-                        else:
-                            print("Selección no válida o conjuntos no disponibles.")
+                    if seleccion == '1':
+                        print(f"Diferencia A - B: {op.diferencia(conjuntoA, conjuntoB)}")
+                    elif seleccion == '2':
+                        print(f"Diferencia B - A: {op.diferencia(conjuntoB, conjuntoA)}")
+                    elif seleccion == '3' and conjuntoC:
+                        print(f"Diferencia A - C: {op.diferencia(conjuntoA, conjuntoC)}")
+                    elif seleccion == '4' and conjuntoC:
+                        print(f"Diferencia B - C: {op.diferencia(conjuntoB, conjuntoC)}")
+                    elif seleccion == '5' and conjuntoC:
+                        print(f"Diferencia C - A: {op.diferencia(conjuntoC, conjuntoA)}")
+                    elif seleccion == '6' and conjuntoC:
+                        print(f"Diferencia C - B: {op.diferencia(conjuntoC, conjuntoB)}")
                     else:
-                        print("Necesitas al menos los conjuntos A y B para esta operación.")
+                        print("Selección no válida o conjuntos no disponibles.")
 
+                # Realiza operacion de diferencia simétrica
                 elif operation == '6':
                     print("Seleccione los conjuntos para la diferencia simétrica:")
                     if conjuntoA and conjuntoB:
-                        print("1. Diferencia Simétrica A - B")
-                        if conjuntoC:
-                            print("2. Diferencia Simétrica A - C")
-                            print("3. Diferencia Simétrica B - C")
+                        print("1. Diferencia Simétrica A Δ B")
+                    if conjuntoA and conjuntoC:
+                        print("2. Diferencia Simétrica A Δ C")
+                    if conjuntoB and conjuntoC:
+                        print("3. Diferencia Simétrica B Δ C")
 
-                        seleccion = input("> Ingrese el número de la operación: ")
+                    seleccion = input("> Ingrese el número de la operación: ")
 
-                        if seleccion == '1':
-                            print(f"Diferencia Simétrica A - B: {op.diferencia_simetrica(conjuntoA, conjuntoB)}")
-                        elif seleccion == '2' and conjuntoC:
-                            print(f"Diferencia Simétrica A - C: {op.diferencia_simetrica(conjuntoA, conjuntoC)}")
-                        elif seleccion == '3' and conjuntoC:
-                            print(f"Diferencia Simétrica B - C: {op.diferencia_simetrica(conjuntoB, conjuntoC)}")
-                        else:
-                            print("Selección no válida o conjuntos no disponibles.")
+                    if seleccion == '1':
+                        print(f"Diferencia Simétrica A Δ B: {op.diferencia_simetrica(conjuntoA, conjuntoB)}")
+                    elif seleccion == '2' and conjuntoC:
+                        print(f"Diferencia Simétrica A Δ C: {op.diferencia_simetrica(conjuntoA, conjuntoC)}")
+                    elif seleccion == '3' and conjuntoC:
+                        print(f"Diferencia Simétrica B Δ C: {op.diferencia_simetrica(conjuntoB, conjuntoC)}")
                     else:
-                        print("Necesitas al menos los conjuntos A y B para esta operación.")
-                    limpiar_consola()
+                        print("Selección no válida o conjuntos no disponibles.")
+
+
                 else:
                     print("Operación no válida.")
-
-
+                    limpiar_consola()
+     
         elif choice == '3':
             print("Saliendo del programa...")
             break
         
         else:
             print("Opción no válida")
+            limpiar_consola()
 
 if __name__ == "__main__":
     menu()
--- src/validator.py
+++ src/validator.py
@@ -1,18 +1,26 @@
+# Definir el universo de números y letras
 numberUniverse = set(map(str, range(0, 10)))
 lettersUniverse = set(chr(c) for c in range(ord('A'), ord('Z') + 1))
 validUniverse = numberUniverse.union(lettersUniverse)
 
+#Valida los elementos de un array, asegurando que sean parte del universo válido (números o letras).
 def universeValidator(array):
     valid_elements = []
     for element in array:
+        # Verifica si el elemento es válido
         if len(element) == 1 and element.upper() in validUniverse:
+            # Agrega el elemento válido en mayúsculas
             valid_elements.append(element.upper())
         else:
+            # Mensaje de error
             print(f"Elemento no válido encontrado: {element}")
     return valid_elements
 
+
+# Solicita al usuario que ingrese un conjunto de elementos, los valida y los convierte a mayúsculas.
 def ingresarConjunto(nombre_conjunto):
     conjunto = input(f"Ingrese el conjunto {nombre_conjunto} (separado por comas):").upper()  # Convertir todo a mayúsculas
+     # Verifica que el conjunto no esté vacío
     if not conjunto:
         print(f"El conjunto {nombre_conjunto} no puede estar vacío.")
         return None
@@ -23,6 +31,7 @@ def ingresarConjunto(nombre_conjunto):
     return conjunto_validado
 
 def construirConjuntos():
+    # Pide ingresar dos o tres conjuntos, los valida y los retorna
     conjuntoA = ingresarConjunto("A")
     conjuntoB = ingresarConjunto("B")
 
@@ -32,4 +41,5 @@ def construirConjuntos():
     else:
         conjuntoC = None
 
+    # Retorno de conjuntos
     return conjuntoA, conjuntoB, conjuntoC
